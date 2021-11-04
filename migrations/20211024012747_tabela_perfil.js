2037cc0dfaa85389ac6b911323d70fa968393cc6
Felipe Pereira <felipe.pereira@CG.LOCAL>
2021-11-03 23:20:20 -0300

    nova aula

--- migrations/20211024012747_tabela_perfil.js
+++ migrations/20211024012747_tabela_perfil.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex, promise) {
+    return knex.schema.createTable('perfis', table => {
+        table.increments('id').primary()
+        table.string('nome').notNull().unique()
+        table.string('rotulo').notNull()
+    }).then(function () {
+        return knex('perfis').insert([
+            { nome: 'comum', rotulo: 'Comum' },
+            { nome: 'admin', rotulo: 'Administrador' },
+            { nome: 'master', rotulo: 'Master' }
+        ])
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex, promise) {
+    return knex.schema.dropTable('perfis')
 };
